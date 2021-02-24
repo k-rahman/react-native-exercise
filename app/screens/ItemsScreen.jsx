407187e682c4556b25268f361b7824044b4214fe
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-02-24 14:02:08 +0200

    Create HomeScreen where user can browse items/listings

--- app/screens/ItemsScreen.jsx
+++ app/screens/ItemsScreen.jsx
@@ -1,63 +1,67 @@
-import React, { useEffect } from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import React, { useState, useCallback, useEffect } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import { FlatList, StyleSheet } from "react-native";
 
 import ActivityIndicator from "../components/ActivityIndicator";
-import AppText from "../components/AppText";
-import AppButton from "../components/AppButton";
 import Card from "../components/Card";
 import colors from "../config/colors";
 import itemsApi from "../api/items";
 import NotFound from "../components/NotFound";
 import useApi from "../hooks/useApi";
 import Screen from "../components/Screen";
-import { useState } from "react";
 
 const itemsScreen = ({ navigation, route }) => {
   const [refresh, setRefresh] = useState(false);
-  const { data: items, error, loading, request: loadItems } = useApi(
+  const { response, error, loading, request: loadItems } = useApi(
     itemsApi.getItems
   );
+  const { data: items } = response;
 
-  useEffect(() => {
-    loadItems(route.params);
-  }, []);
+  useFocusEffect(
+    useCallback(() => {
+      loadItems(route.params);
+
+      return () => {};
+    }, [])
+  );
 
   return (
-    <View style={styles.container}>
-      {items.code === "404" && <NotFound message={items.message} />}
-      {error && (
+    <Screen style={styles.container}>
+      {error ? (
         <>
-          <AppText>Couldnt retrieve the listings.</AppText>
-          <AppButton title="Retry" onPress={loadItems} />
-        </>
-      )}
-      <ActivityIndicator visible={!refresh && loading} />
-      <FlatList
-        data={items}
-        keyExtractor={item => item.id.toString()}
-        refreshing={refresh}
-        onRefresh={() => loadItems(route.params)}
-        renderItem={({ item }) => (
-          <Card
-            title={item.title}
-            subTitle={`${item.price} €`}
-            date={new Date(item.createdAt)
-              .toLocaleString("fi-FI", {
-                timeStyle: "short",
-                dateStyle: "long",
-              })
-              .replace("2021", "")
-              .replace("klo", "")}
-            image={item.images[0]}
-            onPress={() => {
-              route.params
-                ? navigation.navigate("SearchItemDetails", item)
-                : navigation.navigate("ItemDetails", item);
-            }}
+          <NotFound
+            response={response}
+            retry={loadItems}
+            params={route.params}
           />
-        )}
-      />
-    </View>
+          <ActivityIndicator visible={!refresh && loading} />
+        </>
+      ) : (
+        <FlatList
+          data={items}
+          keyExtractor={item => item.id.toString()}
+          refreshing={refresh}
+          onRefresh={() => loadItems(route.params)}
+          renderItem={({ item }) => (
+            <Card
+              title={item.title}
+              subTitle={`${item.price} €`}
+              date={new Date(item.createdAt)
+                .toLocaleString("fi-FI", {
+                  timeStyle: "short",
+                  dateStyle: "long",
+                })
+                .replace("2021", "")
+                .replace("klo", "")}
+              image={item.images[0]}
+              onPress={() => {
+                navigation.navigate("itemDetails", item);
+              }}
+            />
+          )}
+        />
+      )}
+    </Screen>
   );
 };
 
