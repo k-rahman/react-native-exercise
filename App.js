6f5cc8cb515e8898272c777875054dcc29d9fc05
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-02-28 12:50:43 +0200

    Implement Final App.js

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { decode, encode } from "base-64";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>React Native</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+if (!global.btoa) {
+  global.btoa = encode;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+if (!global.atob) {
+  global.atob = decode;
+}
+
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import AppLoading from "expo-app-loading";
+import AuthContext from "./app/auth/context";
+import authStorage from "./app/auth/storage";
+import navigationTheme from "./app/navigation/navigationTheme";
+import MainNav from "./app/navigation/MainNav";
+
+export default function App() {
+  const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
+
+  const restoreUser = async _ => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
+  };
+
+  if (!isReady)
+    return (
+      <AppLoading
+        startAsync={restoreUser}
+        onError={e => console.log(e)}
+        onFinish={() => setIsReady(true)}
+      />
+    );
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      <NavigationContainer theme={navigationTheme}>
+        <MainNav />
+      </NavigationContainer>
+    </AuthContext.Provider>
+  );
+}
